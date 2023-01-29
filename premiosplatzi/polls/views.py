f40284c421f6bc4fdfd63191868baffadaee97dd
alejopanto <jose.pantoja@testu.co>
2023-01-29 15:53:33 -0500

    create views and routes

--- premiosplatzi/polls/views.py
+++ premiosplatzi/polls/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Hello world")
-# Create your views here.
+    return HttpResponse("Estas en la pagina principal")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"estas viendo la pregunta numero {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"estas viendo los reslutados de la pregunta numero {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"estas votando a la pregunta numero {question_id}")
