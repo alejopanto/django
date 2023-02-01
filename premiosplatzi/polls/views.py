d901fcc889de74f02e35bd1d8aef7925822f51f7
alejopanto <jose.pantoja@testu.co>
2023-01-31 21:28:21 -0500

    view results

--- premiosplatzi/polls/views.py
+++ premiosplatzi/polls/views.py
@@ -19,7 +19,10 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"estas viendo los reslutados de la pregunta numero {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {
+        "question": question
+    })
 
 
 def vote(request, question_id):
