f240c09a0092749bd7664e64cee4af17265550fe
alejopanto <jose.pantoja@testu.co>
2023-02-07 23:05:32 -0500

    render and template

--- platzigram/posts/views.py
+++ platzigram/posts/views.py
@@ -1,36 +1,39 @@
 #Django
-from django.http import HttpResponse
+from django.shortcuts import render
 
 #Utilities
 from datetime import datetime
 
 posts = [
-    {
-        'name': 'Moct Blac',
-        'user': 'Jessica Cortes',
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1036'
-    },
-    {
-        'name': 'Via Lactea',
-        'user': 'C, vander',
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=903'
-    },
-    {
-        'name': 'Nuevo auditorio',
-        'user': 'Fulanito de tal',
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1076'
-    }
+	{
+		'title': 'Mont Blanc',
+		'user': {
+			'name': 'Yésica Cortés',
+			'picture': 'https://picsum.photos/60/60/?image=1027'
+		},
+		'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+		'photo': 'https://picsum.photos/800/600?image=1036',
+	},
+	{
+		'title': 'Via Láctea',
+		'user': {
+			'name': 'Christian Van der Henst',
+			'picture': 'https://picsum.photos/60/60/?image=1005'
+		},
+		'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+		'photo': 'https://picsum.photos/800/800/?image=903',
+	},
+	{
+		'title': 'Nuevo auditorio',
+		'user': {
+			'name': 'Uriel (thespianartist)',
+			'picture': 'https://picsum.photos/60/60/?image=883'
+		},
+		'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+		'photo': 'https://picsum.photos/500/700/?image=1076',
+	}
 ]
 
 def list_posts(request):
-    content = []
-    for post in posts:
-        content.append("""
-            <p><strong>{name}</strong></p>
-            <p><strong>{user}</strong></p>
-            <figure><img src="{picture}"></figure>
-        """.format(**post))
-    return HttpResponse('<br>'.join(content))
+    
+    return render(request, 'feed.html', {'posts': posts})
